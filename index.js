a478e8a2e41e653244bd10060ff2701668f426cb
github username <github email address>
2019-10-16 14:39:29 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your functions here
+function map(arr, fcn){
+    let result = [];
+    for(let i = 0; i < arr.length; i++){
+        result.push(fcn(arr[i]))
+    }
+    return result
+}
+
+function reduce(arr, fcn, start){
+
+    let result = (!!start) ? start : arr[0];
+    let i = (!!start) ? 0 : 1;
+    for(; i < arr.length; i++){
+        result = fcn(arr[i], result)
+    }
+    return result
+}
