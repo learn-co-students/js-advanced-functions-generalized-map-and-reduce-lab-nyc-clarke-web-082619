780618b0aafdbb55602dd0252e75db7c03ba206c
castagnaTM <castagna.tm@gmail.com>
2019-10-16 12:28:47 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your functions here
+function map(sourceArray, fn){
+    let newArr = [];
+    for(let i = 0; i < sourceArray.length; i++){
+        newArr.push(fn(sourceArray[i]));
+    }
+    return newArr;
+}
+
+function reduce(someArray, fn, startingPoint=0){
+    let total = someArray[0];  
+    if (startingPoint){
+        total = startingPoint + total;
+    }
+    for(let i = 1;i<someArray.length; i++){
+        total = fn(someArray[i], total);
+    }
+    
+    return total;
+}
